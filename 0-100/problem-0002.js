399d0fb43e1ee3db2c734a6f25db978456c22e54
honmanyau <honmanyau@users.noreply.github.com>
2018-05-14 15:57:47 +1000

    Added solution to Problem 2.

---
+++ 0-100/problem-0002.js
@@ -0,0 +1,23 @@
+/**************/
+/* Problem 2: */
+/**************/
+
+function sumOfEvenFibonacciTerms(limit, i = 0, j = 1, sum = 0) {
+  const nextTerm = i + j;
+
+  // Check if the current term is even, if so, add it to sum
+  if (j % 2 === 0) {
+    sum += j;
+  }
+
+  // Terminate recursion and return sum if the current term is > limit
+  if (nextTerm > limit) {
+    return sum;
+  }
+
+  return sumOfEvenFibonacciTerms(limit, j, nextTerm, sum);
+}
+
+console.log(`Solution: ${sumOfEvenFibonacciTerms(4000000)}.`)
+
+module.exports = { sumOfEvenFibonacciTerms };
