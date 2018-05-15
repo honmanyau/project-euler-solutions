a23392d590a26f1e415321a94e8003ed188a850a
honmanyau <honmanyau@users.noreply.github.com>
2018-05-15 21:53:21 +1000

    Added solution to Problem 12.

---
+++ 0-100/problem-0012.js
@@ -0,0 +1,66 @@
+/**************/
+/* Problem 12 */
+/**************/
+
+function findAllFactorsOf(num) {
+  let factors = [];
+  // Only need to calculate up to √n as pairs of composites are mirrored
+  // after that
+  for (let i = 1, l = Math.sqrt(num); i <= l; i++) {
+    const isFactor = num % i === 0;
+
+    if (isFactor) {
+      const quotient = num / i;
+
+      if (i !== quotient) {
+        factors.push(quotient);
+      }
+
+      factors.push(i)
+    }
+  }
+
+  return factors.sort((a, b) => a - b);
+}
+
+// Deprecated
+// function generateTriangleNumbers(n) {
+//   const indicies = Array.from(Array(n + 1)).map((x, index) => index);
+//   const triangularNumbers = indicies.slice(0);
+//
+//   for (let i = 1, l = indicies.length; i < l; i++) {
+//     triangularNumbers[i] = triangularNumbers[i] + triangularNumbers[i - 1];
+//   }
+//
+//   triangularNumbers.shift();
+//
+//   return triangularNumbers;
+// }
+
+function generateNthTriangularNumber(n) {
+  return n * (n + 1) / 2;
+}
+
+function findFirstTriangleNumberWithNumberOfFactors(n) {
+  let triangularNumber;
+  let factors = [];
+
+  for (let i = 1; factors.length <= n; i++) {
+    triangularNumber = generateNthTriangularNumber(i);
+    factors = findAllFactorsOf(triangularNumber);
+    console.log(i, triangularNumber, factors.length);
+  }
+
+  return triangularNumber;
+}
+
+const init = process.hrtime();
+console.log(`Solution: ${findFirstTriangleNumberWithNumberOfFactors(500)}`);
+const final = process.hrtime(init);
+console.log(`Time taken: ${final[0] * 1000 + final[1] / 1000000} ms.`);
+
+module.exports = {
+  findAllFactorsOf,
+  generateNthTriangularNumber,
+  findFirstTriangleNumberWithNumberOfFactors
+};
---
+++ test/problem-0012.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const functions = require('../0-100/problem-0012.js');
+
+describe('Problem 12 functions:', () => {
+  describe('findAllFactorsOf(num)', () => {
+    const { findAllFactorsOf } = functions;
+
+    it([
+      `should return [1, 2, 4, 7, 14, 28] for num = 28.`
+    ].join(''), () => {
+      assert.equal(findAllFactorsOf(28).toString(), '1,2,4,7,14,28');
+    });
+  });
+
+  describe('generateNthTriangularNumber(n)', () => {
+    const { generateNthTriangularNumber } = functions;
+
+    it([
+      `should return 28 for n = 7.`
+    ].join(''), () => {
+      assert.equal(generateNthTriangularNumber(7).toString(), 28);
+    });
+  });
+
+  describe('findFirstTriangleNumberWithNumberOfFactors(n)', () => {
+    const { findFirstTriangleNumberWithNumberOfFactors } = functions;
+
+    it([
+      `should return 6 for n = 4`
+    ].join(''), () => {
+      assert.equal(findFirstTriangleNumberWithNumberOfFactors(4), 6);
+    });
+  });
+});
